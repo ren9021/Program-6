ad34b6a435dab115cbeea052728667a50acfb4aa
Renee White <43598859+ren9021@users.noreply.github.com>
2020-05-21 16:07:45 -0500

    Add files via upload

---
+++ Program6.py
@@ -0,0 +1,95 @@
+#***************************************************************
+#
+#  Developer:         Renee White
+#
+#  Program #:         Program 6
+#
+#  File Name:         Program6.py
+#
+#  Course:            COSC 1336 Programming Fundamentals I 
+#
+#  Due Date:          10-13-19
+#
+#  Instructor:        Fred Kumi 
+#
+#  Chapter:           Chapter #s 1-4
+#
+#  Description:
+#     This is a program that calculates the occupancy rate for a hotel.
+#
+#***************************************************************
+
+#***************************************************************
+#
+#  Function:     main
+# 
+#  Description:  The main function of the program
+#
+#  Parameters:   None
+#
+#  Returns:      Nothing 
+#
+#**************************************************************
+def main():
+    
+    developerInfo()
+    
+    print('Welcome to your hotel management system.')
+    print('')
+    floors = int(input('How many floors does your hotel have?: '))
+    print('')
+    TotalRooms = 0
+    TotalOcc = 0
+    for floor in range(floors):
+        print('Floor number', floor + 1)
+        print('-----------------')
+        NumRooms = int(input('How many rooms are on this floor?: '))
+        TotalRooms = TotalRooms + NumRooms
+        occ = int(input('How many rooms are occupied on this floor?: '))
+        print('')
+        print('')
+        TotalOcc = TotalOcc + occ
+        NotOcc = TotalRooms - TotalOcc
+        Percentage = TotalOcc / TotalRooms
+
+            
+
+    print('')
+    print('')
+    print('Your hotel has a total of', TotalRooms, 'rooms')
+    print('Your hotel has a total of', TotalOcc, 'occupied rooms')
+    print('Your hotel has a total of', NotOcc, 'unoccupied rooms available')
+    print('Your hotel has a total occupancy percentage of', format(Percentage, '.0%'))
+    
+            
+    
+        
+            
+             
+        
+
+    
+    # End of the main function 
+    
+#***************************************************************
+#
+#  Function:     developerInfo
+# 
+#  Description:  Prints Programmer's information
+#
+#  Parameters:   None
+#
+#  Returns:      Nothing 
+#
+#**************************************************************
+def developerInfo():
+    print('Name:     Renee White')
+    print('Course:   Programming Fundamentals I')
+    print('Program:  Six')
+    print()
+    # End of the developerInfo function
+
+# Call the main function
+main()
+
+# End of Program 1
